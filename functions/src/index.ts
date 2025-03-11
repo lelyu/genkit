2c4704bf87f5cce0f9459158e7597c28713791bb
lok-lyu <lyulelok@gmail.com>
2025-03-11 18:14:51 -0400

    big time

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,15 @@
-import { gemini15Flash, googleAI } from "@genkit-ai/googleai";
-import {genkit} from "genkit/beta"
+import {gemini15Flash, googleAI} from "@genkit-ai/googleai";
+import {genkit} from "genkit/beta";
 import {z} from "genkit";
 import {onCallGenkit, hasClaim} from "firebase-functions/https";
 import {defineSecret} from "firebase-functions/params";
+
 const googleAIapiKey = defineSecret("GEMINI_API_KEY");
 const systemPrompt =
-    "You are Kian, an assistant for an " +
+  "You are Kian, an assistant for an " +
   "AI documentation tool called DocIt.";
 
-// configure a Genkit instance
+// Configure a Genkit instance
 const ai = genkit({
   plugins: [googleAI()],
   model: gemini15Flash,
@@ -20,12 +21,14 @@ const ai = genkit({
  * @param {{prompt: string}} data - An object containing the prompt.
  * @return {Promise<string>} The generated response text.
  */
-async function generateResponseFromGemini(data: {prompt: string}) {
-    const { prompt } = data;
-    
+async function generateResponseFromGemini(
+  data: {prompt: string}
+): Promise<string> {
+  const {prompt} = data;
+
   const {text} = await ai.generate({
     system: systemPrompt,
-    prompt: prompt,
+    prompt,
     config: {
       maxOutputTokens: 400,
       stopSequences: ["<end>", "<fin>"],
@@ -45,23 +48,20 @@ const summarizeDataFlow = ai.defineFlow(
   },
   async ({prompt}, {context}) => {
     // if (!context.auth?.uid) throw new Error("Must supply auth context.");
-    const res = await generateResponseFromGemini({ prompt: prompt });
-    if (context) {
-      console.log(context.auth.uid)
+    const res = await generateResponseFromGemini({prompt});
+    if (context && context.auth && context.auth.uid) {
+      console.log(context.auth.uid);
     }
-    console.log('hello from summarize Data Flow')
-    console.log(res)
+    console.log("hello from summarize Data Flow");
+    console.log(res);
     return res;
   }
 );
 
-
-const summary = summarizeDataFlow({ prompt: "What is your name" }, {
-  context: {
-    auth: {
-      uid: "a test uid"
-}}});
-
+// const summary = summarizeDataFlow(
+//   {prompt: "What is your name"},
+//   {context: {auth: {uid: "a test uid"}}}
+// );
 
 export const summarizeData = onCallGenkit(
   {
